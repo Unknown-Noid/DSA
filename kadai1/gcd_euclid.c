b9083e51d1131d2d6c873e4ad7732bca9489d110
Sakuya <83571119+Unknown-Noid@users.noreply.github.com>
2024-10-13 06:53:10 +0900

    関数を記述した

--- kadai1/gcd_euclid.c
+++ kadai1/gcd_euclid.c
@@ -4,7 +4,20 @@
 // Find the greatest common divisor of the two integers, n and m.
 int gcd_euclid(int n, int m) {
 
-    // ŠÖ”‚ğŠ®¬‚³‚¹‚æ
+    if (n < m) {
+        int tmp;
+        n = tmp;
+        n = m;
+        m = tmp;
+    }
+    int i = 1;
+    int gcd;
+    while (i <= n) {
+        if (n%i == 0 && m%i == 0)
+            gcd = i;
+        i = i+1;
+    }
+
 
     return n;
 }
