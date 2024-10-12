ec91d57e8c8c8c8bbad998948135c68baa0da59c
Sakuya <dai.dai.daidaidai@icloud.com>
2024-10-13 06:15:29 +0900

    dsa commit

---
+++ kadai1/gcd_euclid.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Find the greatest common divisor of the two integers, n and m.
+int gcd_euclid(int n, int m) {
+
+    // ŠÖ”‚ğŠ®¬‚³‚¹‚æ
+
+    return n;
+}
